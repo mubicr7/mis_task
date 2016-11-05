24f2cc5f9cbab04d32795aa9e35dd2fffbbd44dd
Zain Aftab <Zain Aftab>
2016-11-05 14:45:34 +0500

    added code file

---
+++ zaincode123.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void add()
+{
+	int a;
+	int b;
+	cin>>a;
+	cin>>b;
+	cout<<"Addition is:";	
+	cout<<a+b<<endl;
+	cout<<"Zain Aftab, 140333";
+}
+
+int main()
+{
+	
+}
